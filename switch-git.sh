7c26ff71206cfb86a947c569209f6bb7ad2d8fc7
meeeed23-maker <meeeed23@gmail.com>
2026-05-15 16:18:58 +0100

    chore:create project structure

--- switch-git.sh
+++ switch-git.sh
@@ -1,14 +1,21 @@
-@echo off
-if "%1"=="lead" (
+#!/bin/bash
+
+case "$1" in
+  lead)
     git config user.name "meeeed23-maker"
     git config user.email "meeeed23@gmail.com"
-    echo Switched to LEAD account
-) else if "%1"=="reviewer" (
-    git config user.name "mhamedachbani "
-    git config user.email "mhamed.achbani@uit.ac.ma"
-    echo Switched to REVIEWER account
-) else (
-    echo Usage: switch-git.bat {lead^|reviewer}
-)
+    echo "✅ Switched to LEAD account"
+    ;;
+  reviewer)
+    git config user.name "mhamedachbani"
+    git config user.email "hamed.achbani@uit.ac.ma"
+    echo "✅ Switched to REVIEWER account"
+    ;;
+  *)
+    echo "Usage: ./switch-git.sh {lead|reviewer}"
+    ;;
+esac
+
+echo "Current user:"
 git config user.name
 git config user.email
